c552971172ef8fb359f71b1d9615f25bf1299a94
Coder-nandni <nandni141144149@gmail.com>
2026-07-12 12:43:37 +0530

    feat: added AssetFlow authentication UI with login signup routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-5xl font-bold text-orange-500">
-        AssetFlow 🚀
-      </h1>
-    </div>
-  );
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+
+
+function App(){
+
+return(
+
+<Routes>
+
+<Route path="/" element={<Signup/>}/>
+
+<Route path="/signup" element={<Signup/>}/>
+
+<Route path="/login" element={<Login/>}/>
+
+</Routes>
+
+)
+
 }
+
 
 export default App;
